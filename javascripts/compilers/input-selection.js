68985a55b2716610d7c24c8ed1f0fc0634795d4a
Ichaelus <Ichaelus@users.noreply.github.com>
2020-11-18 14:41:34 +0100

    Add track look to the switch

--- javascripts/compilers/input-selection.js
+++ javascripts/compilers/input-selection.js
@@ -1,13 +1,21 @@
 up.compiler('.input-selection', function(element, data){
 
+  let enabled = false; // Only react to input events if the machine is powered on
+
   up.on('audioContext:connected', function(){
-    up.on(element, 'click', '.input-selection--switch-knob', function(){
-      InputController.switchToNextInput();
+    up.on(element, 'click', '.input-selection--switch', function(){
+      if(enabled){
+        InputController.switchToNextInput();
+      }
     });
     
     up.on('plug-in', function(_evt, target){
-      element.classList.toggle('-right', target.classList.contains('microphone'))
+      if(enabled){
+        element.classList.toggle('-right', target.classList.contains('microphone'))
+      }
     });
   });
 
+  up.on('reset:on', function(){ enabled = true; })
+  up.on('reset:off', function(){ enabled = false; })
 });
