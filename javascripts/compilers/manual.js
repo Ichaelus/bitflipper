88b45d0dccba7d200d88e249bee7e647b5bfcd97
Ichaelus <Ichaelus@users.noreply.github.com>
2022-07-24 20:29:52 +0200

    Add manual link

---
+++ javascripts/compilers/manual.js
@@ -0,0 +1,24 @@
+up.compiler('.manual', (element) => {
+
+  const helpLink = document.querySelector('.footer--link.-manual')
+
+  let manualVisible = false
+
+  function toggleManual() {
+    if (manualVisible) {
+      up.element.hide(element)
+      document.querySelector('.machine').classList.toggle('-flipped')
+      helpLink.textContent = 'Show manual'
+    } else {
+      document.querySelector('.machine').classList.toggle('-flipped')
+      up.element.show(element)
+      helpLink.textContent = 'Hide manual'
+    }
+    manualVisible = !manualVisible
+  }
+
+  return [
+    up.on(helpLink, 'click', toggleManual)
+  ]
+
+})
