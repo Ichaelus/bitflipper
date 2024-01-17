66903e57abfab74210985f0c19c02d57e8dbcb8d
Ichaelus <Ichaelus@users.noreply.github.com>
2024-01-17 20:21:42 +0100

    Fix input selection before powering on

--- javascripts/compilers/local-audio-source.js
+++ javascripts/compilers/local-audio-source.js
@@ -1,6 +1,11 @@
 up.compiler('.local-audio-source', (fileInput) => {
   fileInput.onchange = (evt) => {
     up.emit('audio-input:use-file', { file: fileInput.files[0] })
-    
+  }
+
+  input.onclick = function (evt) {
+    // we wouldn't get a change event otherwise
+    // that's especially bad if we previously tried to select a file before powering on
+    evt.target.value = null
   }
 })
